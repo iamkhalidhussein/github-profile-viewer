16d83a2573b95271adab914d13cf9b43de58bf7c
khalid09dev <mdkhalidhossen@hotmail.com>
2025-01-13 16:46:36 +0600

    added copilot image

--- src/components/SearchNBanner.jsx
+++ src/components/SearchNBanner.jsx
@@ -4,9 +4,14 @@ import BannerTitle from './BannerTitle';
 
 const SearchNBanner = () => {
     return (
-        <div className='absolute bottom-1/2'>
-            <BannerTitle/>
-            <Search/>
+        <div className='absolute bottom-1/4 flex items-center'>
+            <div>
+                <BannerTitle/>
+                <Search/>
+            </div>
+            <div>
+                <img src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExdjg5cTZ1MjR5d296anQ1d2wzbnV3Z21ueXFlNGM1OHducGpkeGxlZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/Sh1iCtJZEdx4PFYy4q/giphy.webp" alt="copilot" className='select-none' draggable={false}/>
+            </div>
         </div>
     );
 };
