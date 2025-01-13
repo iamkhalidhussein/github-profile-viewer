103f7968780d3441b3df151939f325ccaa1a451c
khalid09dev <mdkhalidhossen@hotmail.com>
2025-01-13 13:10:11 +0600

    added a simple navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import { useKindeAuth } from '@kinde-oss/kinde-auth-react';
+import setUserToken from '../hooks/setUserToken';
+import { Button } from "@/components/ui/button"
+import Logo from './Logo';
+import { useEffect } from 'react';
+
+const Navbar = () => {
+    const { login, register, user } = useKindeAuth();
+    const { setUser } = setUserToken();
+
+    useEffect(() => {
+        setUser();
+    }, [register]);
+
+    return (
+        <div className='flex items-center justify-between mx-20'>
+            <Logo/>
+            <div>
+                <Button variant="secondary">Register</Button>
+                <Button variant="primary">Login</Button>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
